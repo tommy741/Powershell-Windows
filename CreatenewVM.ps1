0e1e7bdae4ff13ecc9738ff78b9c8d7a1a19ba2a
tommy741 <tom-reichert@hotmail.de>
2018-07-03 11:49:39 +0200

    Update CreatenewVM.ps1

--- CreatenewVM.ps1
+++ CreatenewVM.ps1
@@ -1,4 +1,6 @@
-$VMname = "test"
+$VMname = Read-Host 'Bitte geben Sie Rechnernamen ein'
+$Srviso = "C:\iso\SW_DVD9_Win_Server_STD_CORE_2016_64Bit_English_-4_DC_STD_MLF_X21-70526.ISO"
+#$Srviso = "C:\iso\en_windows_10_enterprise_x64_dvd_6851151.iso"
 
 $NewVMParam = @{
   Name = $VMname
@@ -11,4 +13,7 @@ $NewVMParam = @{
   Verbose =  $True
   }
 
-$VM = New-VM @NewVMParam 
+
+$VM = New-VM @NewVMParam
+Set-VMDvdDrive -VMName $VMname -Path $Srviso
+Start-vm $VMname

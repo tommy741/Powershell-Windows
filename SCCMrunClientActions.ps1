eafa66a6527a19fd6f6ac971a65ec1b8bc6af173
tommy741 <tom-reichert@hotmail.de>
2018-07-16 09:52:25 +0200

    Create SCCMrunClientActions.ps1

---
+++ SCCMrunClientActions.ps1
@@ -0,0 +1,12 @@
+$SCCMClient = New-Object -COM 'CPApplet.CPAppletMgr'
+
+$SCCMClient.GetClientActions()[1].PerformAction()
+$SCCMClient.GetClientActions()[2].PerformAction()
+$SCCMClient.GetClientActions()[3].PerformAction()
+$SCCMClient.GetClientActions()[4].PerformAction()
+$SCCMClient.GetClientActions()[5].PerformAction()
+$SCCMClient.GetClientActions()[6].PerformAction()
+$SCCMClient.GetClientActions()[7].PerformAction()
+$SCCMClient.GetClientActions()[8].PerformAction()
+$SCCMClient.GetClientActions()[9].PerformAction()
+$SCCMClient.GetClientActions()[10].PerformAction()
